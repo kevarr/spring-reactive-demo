89a4de7a9c976c2877662ea397814e4634d1a250
kevinarroyo <kevin.israel.arroyo@gmail.com>
2021-07-17 13:32:30 -0400

    configure application

---
+++ src/main/kotlin/com/kevinarroyo/springreactivedemo/SpringReactiveDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.kevinarroyo.springreactivedemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringReactiveDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringReactiveDemoApplication>(*args)
+}
